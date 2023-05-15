562249d905d1e35b2193bc2c834feef30efdba9e
Gael Nogawa <123341163+Gael-Nogawa@users.noreply.github.com>
2023-05-15 15:27:35 -0300

    finalizando

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ $(document).ready(function(){
 
     $('header button').click(function(e){
         $('.container').slideDown();
-
     })
     
-    $('#recolher').click(function(){
+    $('#recolher').click(function(e){
         $('.container').slideUp();
     })
 
@@ -13,20 +12,14 @@ $(document).ready(function(){
         e.preventDefault();
 
         const inputTarefa = $('#colocando-nova-tarefa').val();
-
-        console.log(inputTarefa)
         const novaTarefa = $(`<li>${inputTarefa}</li>`);
         $(novaTarefa).appendTo('ul');
-        console.log('elemento1');
-        
+  
         $(novaTarefa).on('click', function(){
             const liRisc = $(novaTarefa).toggleClass('riscado');
-            console.log('elemento2')
+            
         })
         $('#colocando-nova-tarefa').val(' ');
        
     })
-
-   
-
 })
